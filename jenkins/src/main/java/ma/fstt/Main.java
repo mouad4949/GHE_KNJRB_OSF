864f76980a06322c3a8da8fbdac1e6df3c9fc2d0
mouad4949 <mouadrguibi900@gmail.com>
2024-12-08 00:31:23 +0100

    3la lah

--- jenkins/src/main/java/ma/fstt/Main.java
+++ jenkins/src/main/java/ma/fstt/Main.java
@@ -2,6 +2,6 @@ package ma.fstt;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Rah khdem bsehtk hbibi");
+        System.out.println("rah khdem ahbibi");
     }
 }
